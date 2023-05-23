fc5ab6bb49d27a1a140e5ca021a02e6d2ea68a62
Ayub test <dagujiev.a@routeam.ru>
2023-05-23 18:43:43 +0300

    HW5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        BonusService service = new BonusService();
-        int expected = 150;
-        long actual = service.calculate(5000, true);
-        System.out.println(expected + " == ? == " + actual);
+        //BonusService service = new BonusService();
+       // int expected = 150;
+        //long actual = service.calculate(5000, true);
+        //System.out.println(expected + " == ? == " + actual);
+
+        while(true)
+        {
+            Scanner console = new Scanner(System.in);
+            System.out.println(console);
+            if (console.equals("enough"))
+            {
+                break;
+            }
+            continue;
+
+        }
+
     }
 }
